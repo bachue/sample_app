32f006f741defc924db76473a20e51bc2383ad24
bachue <bachue.shu@gmail.com>
2011-09-09 17:15:38 +0800

    Done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,4 +53,5 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  ActiveSupport::Dependencies.clear
 end

965e9341b71fc5cf248b83554b24a12abe6b21ec
Bachue Zhou <bachue.shu@gmail.com>
2011-09-12 21:51:11 +0800

    create index for email of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,11 @@
 #
 
 class User < ActiveRecord::Base
-  validates_presence_of :name
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates_presence_of :name, :email
+  validates_length_of :name, :maximum => 50
+  validates_format_of :email, :with => email_regex
+  validates_uniqueness_of :email, :case_sensitive => false
 end

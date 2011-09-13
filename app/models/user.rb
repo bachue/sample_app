6888b521ce468b1ff6e47510c1deaf877213f7ba
Bachue Zhou <bachue.shu@gmail.com>
2011-09-13 11:30:04 +0800

    just a checkpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,14 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_presence_of :name, :email
   validates_length_of :name, :maximum => 50
+  validates_length_of :password, :within => 6..40
   validates_format_of :email, :with => email_regex
   validates_uniqueness_of :email, :case_sensitive => false
+  validates_confirmation_of :password
 end

ebacd9befdf137b75e2f07127d43bc8d23e2e376
Bachue Zhou <bachue.shu@gmail.com>
2011-09-13 16:18:57 +0800

    reset database

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,8 +32,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, submitted_password)
     user = find_by_email email
-    return nil unless user
-    return user if user.has_password? submitted_password
+    user && user.has_password?(submitted_password) ? user : nil
   end
 
   private

7cb7543058b0821ff93bbc4a2ed0e0c7cde238b5
Bachue Zhou <bachue.shu@gmail.com>
2011-09-13 17:44:12 +0800

    User signup complete!

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -86,7 +86,7 @@ describe UsersController do
 
       it "should have a welcome message" do
         post :create, :user => @attr
-        flash[:success].should === /welcome to the sample app/i
+        flash[:success].should =~ /welcome to the sample app/i
       end
     
     end
